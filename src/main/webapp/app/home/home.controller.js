82b98f6d813114bb8f8819947b667fa7456fdfe1
feel7iii <feelwisteria@sina.cn>
2016-08-15 13:13:20 +0800

    :books:

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -33,9 +33,39 @@
 
             }, onSuccess, onError);
             function onSuccess(data) {
-                var ss = JSON3.stringify(data);
-                ss.replaceAll("id", "key");
-                console.log(data);
+                var ss = angular.toJson(data).replace(new RegExp("id","gm"), "key").replace(new RegExp("parentId","gm"), "parent");
+                var data = angular.fromJson(ss);
+                var $ = go.GraphObject.make;
+                var myDiagram =
+                    $(go.Diagram, "myDiagramDiv",
+                        {
+                            initialContentAlignment: go.Spot.Center, // center Diagram contents
+                            "undoManager.isEnabled": true, // enable Ctrl-Z to undo and Ctrl-Y to redo
+                            layout: $(go.TreeLayout,
+                                {angle: 90, layerSpacing: 20})
+                        });
+
+                // the template we defined earlier
+                myDiagram.nodeTemplate =
+                    $(go.Node, "Horizontal",
+                        {background: "#44CCFF"},
+                        $(go.Picture,
+                            {margin: 7, width: 20, height: 20, background: "#00EEEE"},
+                            new go.Binding("source")),
+                        $(go.TextBlock, "Default Text",
+                            {margin: 7, stroke: "white", font: "bold 16px sans-serif"},
+                            new go.Binding("text", "name"))
+                    );
+
+                // define a Link template that routes orthogonally, with no arrowhead
+                myDiagram.linkTemplate =
+                    $(go.Link,
+                        {routing: go.Link.Orthogonal, corner: 5},
+                        $(go.Shape, {strokeWidth: 3, stroke: "#00EEEE"})); // the link shape
+
+                var model = $(go.TreeModel);
+                model.nodeDataArray = data;
+                myDiagram.model = model;
             }
             function onError(error) {
                 AlertService.error(error.data.message);
